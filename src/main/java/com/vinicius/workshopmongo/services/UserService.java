f78147c93e8817953320bd67dbc511e742d6e06b
Vinicius Atilio <vinicius-atilio@outlook.com>
2022-10-14 10:02:21 -0300

    Inserção de usuário com POST

--- src/main/java/com/vinicius/workshopmongo/services/UserService.java
+++ src/main/java/com/vinicius/workshopmongo/services/UserService.java
@@ -1,11 +1,15 @@
 package com.vinicius.workshopmongo.services;
 
+import com.vinicius.workshopmongo.DTO.UserDTO;
 import com.vinicius.workshopmongo.domain.User;
 import com.vinicius.workshopmongo.repository.UserRepository;
+import com.vinicius.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.embedded.undertow.UndertowServletWebServer;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,5 +19,18 @@ public class UserService {
 
     public List<User> findAll(){
         return repo.findAll();
+    }
+
+    public User findById(String id){
+        Optional<User> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj){
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto){
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
