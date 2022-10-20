bcc492a7af715cb1fbcff376ea8dc35fcbe27b1a
Vinicius Atilio <vinicius-atilio@outlook.com>
2022-10-20 14:10:27 -0300

    Consulta simples com query methods

--- src/main/java/com/vinicius/workshopmongo/resources/PostResource.java
+++ src/main/java/com/vinicius/workshopmongo/resources/PostResource.java
@@ -1,13 +1,13 @@
 package com.vinicius.workshopmongo.resources;
 
 import com.vinicius.workshopmongo.domain.Post;
+import com.vinicius.workshopmongo.resources.util.URL;
 import com.vinicius.workshopmongo.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -22,5 +22,14 @@ public class PostResource {
         return ResponseEntity
                 .ok()
                 .body(obj);
+    }
+
+    @GetMapping(value = "/titlesearch")
+    public ResponseEntity<List<Post>> findByTitle(
+            @RequestParam(value = "text", defaultValue = "") String text
+    ){
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 }
