808710128912540c88b433a7bf25269009acb3c5
Vinicius Atilio <vinicius-atilio@outlook.com>
2022-10-21 09:31:58 -0300

    Consulta com vários critérios

--- src/main/java/com/vinicius/workshopmongo/services/PostService.java
+++ src/main/java/com/vinicius/workshopmongo/services/PostService.java
@@ -6,6 +6,8 @@ import com.vinicius.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.Console;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,5 +24,11 @@ public class PostService {
 
     public List<Post> findByTitle(String text){
         return repo.searchTitle(text);
+    }
+
+    public List<Post> fullSearch(String text, Date minDate, Date maxDate){
+        maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+        System.out.println(maxDate);
+        return repo.fullSearch(text, minDate, maxDate);
     }
 }
